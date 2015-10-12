7d14d57ad20bc1c3adb708ee8d36e245eab10970
Tom Guarnery <thomas.guarnery@gmail.com>
2015-10-12 18:37:03 -0400

    New module Challengeboard added, but not working yet

--- mean/config/env/local.example.js
+++ mean/config/env/local.example.js
@@ -5,19 +5,34 @@
 // Use it for your API keys, passwords, etc.
 
 /* For example:
+ ​
+ module.exports = {
+ db: {
+ uri: 'mongodb://localhost/local-dev',
+ options: {
+ user: '',
+ pass: ''
+ }
+ },
+ facebook: {
+ clientID: process.env.FACEBOOK_ID || 'APP_ID',
+ clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+ callbackURL: '/api/auth/facebook/callback'
+ }
+ };
+ */
 
 module.exports = {
   db: {
-    uri: 'mongodb://localhost/local-dev',
+    uri: 'mongodb://justchoose.me:25565/group9a',
     options: {
-      user: '',
-      pass: ''
+      user: 'cyber',
+      pass: 'ninjas'
     }
   },
-  facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/facebook/callback'
+  google: {
+    clientID: process.env.GOOGLE_ID || '603376827845-24agn835cc74v84hc52v9vdco3sb9oda.apps.googleusercontent.com',
+    clientSecret: process.env.GOOGLE_SECRET || 'QpkR-FZkYsacM4WzZqVMMP4H',
+    callbackURL: '/api/auth/google/callback'
   }
 };
-*/
