d279145876536f5c29db5ba406de72763b6f456a
Nicholas Cioli <nicholascioli@gmail.com>
2015-11-18 13:45:40 -0500

    remove erroneous policy
    Forgot to remove this policy when EventCtl got merged into the
    CurrentCtfEvents factory.

--- mean/modules/ctfEvents/server/policies/ctfEvents.server.policy.js
+++ mean/modules/ctfEvents/server/policies/ctfEvents.server.policy.js
@@ -21,9 +21,6 @@ exports.invokeRolesPolicies = function () {
       resources: '/api/ctfEvents/current',
       permissions: '*'
     }, {
-      resources: '/api/ctfEvents/current/eventCtl',
-      permissions: '*'
-    }, {
       resources: '/api/ctfEvents/:ctfEventId',
       permissions: '*'
     }]
