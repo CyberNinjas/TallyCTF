39f5b71cf0c8ffff2d14881cbb5060a577b57a08
Nicholas Cioli <nicholascioli@gmail.com>
2015-11-20 09:54:49 -0500

    implement basic scoreBoard functionality

--- mean/modules/scoreBoard/client/services/scoreBoard.client.service.js
+++ mean/modules/scoreBoard/client/services/scoreBoard.client.service.js
@@ -4,7 +4,7 @@
 angular.module('scoreBoard').factory('ScoreBoard', ['$resource',
   function ($resource) {
     return $resource('api/scoreBoard/:scoreBoardTeamId', {
-      scoreBoardTeamId: '@team'
+      scoreBoardTeamId: '@team._id'
     }, {
       update: {
         method: 'PUT'
