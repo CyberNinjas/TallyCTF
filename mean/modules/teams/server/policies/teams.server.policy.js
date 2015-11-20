ea9cb3543dc59b71600190096fd1049ba83e731b
Nicholas Cioli <nicholascioli@gmail.com>
2015-11-20 10:11:18 -0500

    fix error with teams.server.policy
    There was an erroneous function removed.

--- mean/modules/teams/server/policies/teams.server.policy.js
+++ mean/modules/teams/server/policies/teams.server.policy.js
@@ -56,31 +56,6 @@ exports.invokeRolesPolicies = function () {
   }]);
 };
 
-/**
- * Check If Teams Policy Allows
- */
-exports.isAllowedList = function (req, res, next) {
-  var roles = (req.user) ? req.user.roles : ['guest'];
-  // If a team is being processed and the current user created it then allow any manipulation
-    return next();
-
-  // Check for user roles
-  acl.areAnyRolesAllowed(roles, req.route.path, req.method.toLowerCase(), function (err, isAllowed) {
-    if (err) {
-      // An authorization error occurred.
-      return res.status(500).send('Unexpected authorization error');
-    } else {
-      if (isAllowed) {
-        // Access granted! Invoke next middleware
-        return next();
-      } else {
-        return res.status(403).json({
-          message: 'User is not authorized'
-        });
-      }
-    }
-  });
-};
 exports.isAllowed = function (req, res, next) {
   var roles = (req.user) ? req.user.roles : ['guest'];
   // If a team is being processed and the current user created it then allow any manipulation
