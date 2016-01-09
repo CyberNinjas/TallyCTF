a634ff923cebd64e9a2ebabf12e281db7336c23b
Doug Logan <dlogan@cyberninjas.com>
2016-01-09 11:39:35 -0500

    Fix grunt #12.

--- deploy.cmd
+++ deploy.cmd
@@ -108,9 +108,9 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. Run grunt
 IF EXIST "%DEPLOYMENT_TARGET%/gruntfile.js" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install grunt-cli
+  call !NPM_CMD! install grunt-cli
   IF !ERRORLEVEL! NEQ 0 goto error
-  call :ExecuteCmd /.node_modules/.bin/grunt --no-color --grunt-file "%DEPLOYMENT_TARGET%/gruntfile.js" build
+  call .node_modules/.bin/grunt --no-color --grunt-file %DEPLOYMENT_TARGET%/gruntfile.js build
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
