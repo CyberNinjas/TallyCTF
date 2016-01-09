314f5cadcb181645215a13291fa0f9738de8df8b
Doug Logan <dlogan@cyberninjas.com>
2016-01-08 21:39:48 -0500

    Fix folder structure so Azure will Auto-Deploy

---
+++ modules/users/server/controllers/users.server.controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var _ = require('lodash');
+
+/**
+ * Extend user's controller
+ */
+module.exports = _.extend(
+  require('./users/users.authentication.server.controller.js'),
+  require('./users/users.authorization.server.controller.js'),
+  require('./users/users.password.server.controller.js'),
+  require('./users/users.profile.server.controller.js')
+);
