57c672edf810b88a11bc7311cbd036fd44022522
DouglasLogan <dlogan@cyberninjas.com>
2016-01-15 17:54:19 -0500

    Fix Error

--- deploy.cmd
+++ deploy.cmd
@@ -100,7 +100,7 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install
+  call :ExecuteCmd !NPM_CMD! install --msvs_version=2013
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
