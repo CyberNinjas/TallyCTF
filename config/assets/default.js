942a43cd65596b5e751360cc5f8b3f588e284d8d
Benjamin Carothers <bcarothers@cyberninjas.com>
2016-02-22 10:36:14 -0500

    Updating assets and getting rid of unnecessary files

--- config/assets/default.js
+++ config/assets/default.js
@@ -6,6 +6,7 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/eeh-navigation/dist/eeh-navigation.css',
         'public/lib/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css'
       ],
       js: [
@@ -19,9 +20,11 @@ module.exports = {
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/bootstrap/dist/js/bootstrap.min.js',
         'public/lib/moment/min/moment.min.js',
-        'public/lib/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
-        'public/lib/bootstrap/dist/js/bootstrap.min.js'
+        'public/lib/eeh-navigation/dist/eeh-navigation.js',
+        'public/lib/eeh-navigation/dist/eeh-navigation.tpl.js',
+        'public/lib/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
