225d2a54175e7042d340f22e5988e07dc32ccecf
prabirshrestha <mail@prabir.me>
2016-05-24 10:40:57 -0400

    Adds doc generation and serving to the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * Module dependencies.
  */
@@ -20,22 +19,18 @@ var _ = require('lodash'),
   webdriver_update = require('gulp-protractor').webdriver_update,
   webdriver_standalone = require('gulp-protractor').webdriver_standalone,
   KarmaServer = require('karma').Server;
-
 // Set NODE_ENV to 'test'
 gulp.task('env:test', function () {
   process.env.NODE_ENV = 'test';
 });
-
 // Set NODE_ENV to 'development'
 gulp.task('env:dev', function () {
   process.env.NODE_ENV = 'development';
 });
-
 // Set NODE_ENV to 'production'
 gulp.task('env:prod', function () {
   process.env.NODE_ENV = 'production';
 });
-
 // Nodemon task
 gulp.task('nodemon', function () {
   return plugins.nodemon({
@@ -45,20 +40,17 @@ gulp.task('nodemon', function () {
     watch: _.union(defaultAssets.server.views, defaultAssets.server.allJS, defaultAssets.server.config)
   });
 });
-
 // Watch Files For Changes
 gulp.task('watch', function () {
   // Start livereload
   plugins.livereload.listen();
-
   // Add watch rules
   gulp.watch(defaultAssets.server.views).on('change', plugins.livereload.changed);
   gulp.watch(defaultAssets.server.allJS, ['eslint']).on('change', plugins.livereload.changed);
   gulp.watch(defaultAssets.client.js, ['eslint']).on('change', plugins.livereload.changed);
   gulp.watch(defaultAssets.client.css, ['csslint']).on('change', plugins.livereload.changed);
   gulp.watch(defaultAssets.client.sass, ['sass', 'csslint']).on('change', plugins.livereload.changed);
-
-  if (process.env.NODE_ENV === 'production') {
+  if(process.env.NODE_ENV === 'production') {
     gulp.watch(defaultAssets.server.gulpConfig, ['templatecache', 'eslint']);
     gulp.watch(defaultAssets.client.views, ['templatecache', 'eslint']).on('change', plugins.livereload.changed);
   } else {
@@ -66,117 +58,73 @@ gulp.task('watch', function () {
     gulp.watch(defaultAssets.client.views).on('change', plugins.livereload.changed);
   }
 });
-
 // CSS linting task
 gulp.task('csslint', function (done) {
-  return gulp.src(defaultAssets.client.css)
-    .pipe(plugins.csslint('.csslintrc'))
-    .pipe(plugins.csslint.reporter())
-    .pipe(plugins.csslint.reporter(function (file) {
-      if (!file.csslint.errorCount) {
-        done();
-      }
-    }));
+  return gulp.src(defaultAssets.client.css).pipe(plugins.csslint('.csslintrc')).pipe(plugins.csslint.reporter()).pipe(plugins.csslint.reporter(function (file) {
+    if(!file.csslint.errorCount) {
+      done();
+    }
+  }));
 });
-
 // ESLint JS linting task
 gulp.task('eslint', function () {
-  var assets = _.union(
-    defaultAssets.server.gulpConfig,
-    defaultAssets.server.allJS,
-    defaultAssets.client.js,
-    testAssets.tests.server,
-    testAssets.tests.client,
-    testAssets.tests.e2e
-  );
-
-  return gulp.src(assets)
-    .pipe(plugins.eslint())
-    .pipe(plugins.eslint.format());
+  var assets = _.union(defaultAssets.server.gulpConfig, defaultAssets.server.allJS, defaultAssets.client.js, testAssets.tests.server, testAssets.tests.client, testAssets.tests.e2e);
+  return gulp.src(assets).pipe(plugins.eslint()).pipe(plugins.eslint.format());
 });
-
 // JS minifying task
 gulp.task('uglify', function () {
-  var assets = _.union(
-    defaultAssets.client.js,
-    defaultAssets.client.templates
-  );
-
-  return gulp.src(assets)
-    .pipe(plugins.ngAnnotate())
-    .pipe(plugins.uglify({
-      mangle: false
-    }))
-    .pipe(plugins.concat('application.min.js'))
-    .pipe(gulp.dest('public/dist'));
+  var assets = _.union(defaultAssets.client.js, defaultAssets.client.templates);
+  return gulp.src(assets).pipe(plugins.ngAnnotate()).pipe(plugins.uglify({
+    mangle: false
+  })).pipe(plugins.concat('application.min.js')).pipe(gulp.dest('public/dist'));
 });
-
 // CSS minifying task
 gulp.task('cssmin', function () {
-  return gulp.src(defaultAssets.client.css)
-    .pipe(plugins.cssmin())
-    .pipe(plugins.concat('application.min.css'))
-    .pipe(gulp.dest('public/dist'));
+  return gulp.src(defaultAssets.client.css).pipe(plugins.cssmin()).pipe(plugins.concat('application.min.css')).pipe(gulp.dest('public/dist'));
 });
-
 // Sass task
 gulp.task('sass', function () {
-  return gulp.src(defaultAssets.client.sass)
-    .pipe(plugins.sass())
-    .pipe(plugins.autoprefixer())
-    .pipe(plugins.rename(function (file) {
-      file.dirname = file.dirname.replace(path.sep + 'scss', path.sep + 'css');
-    }))
-    .pipe(gulp.dest('./modules/'));
+  return gulp.src(defaultAssets.client.sass).pipe(plugins.sass()).pipe(plugins.autoprefixer()).pipe(plugins.rename(function (file) {
+    file.dirname = file.dirname.replace(path.sep + 'scss', path.sep + 'css');
+  })).pipe(gulp.dest('./modules/'));
 });
-
 // Angular template cache task
 gulp.task('templatecache', function () {
   var re = new RegExp('\\' + path.sep + 'client\\' + path.sep, 'g');
-
-  return gulp.src(defaultAssets.client.views)
-    .pipe(plugins.templateCache('templates.js', {
-      root: 'modules/',
-      module: 'core',
-      templateHeader: '(function () {' + endOfLine + '	\'use strict\';' + endOfLine + endOfLine + '	angular' + endOfLine + '		.module(\'<%= module %>\'<%= standalone %>)' + endOfLine + '		.run(templates);' + endOfLine + endOfLine + '	templates.$inject = [\'$templateCache\'];' + endOfLine + endOfLine + '	function templates($templateCache) {' + endOfLine,
-      templateBody: '		$templateCache.put(\'<%= url %>\', \'<%= contents %>\');',
-      templateFooter: '	}' + endOfLine + '})();' + endOfLine,
-      transformUrl: function (url) {
-        return url.replace(re, path.sep);
-      }
-    }))
-    .pipe(gulp.dest('build'));
+  return gulp.src(defaultAssets.client.views).pipe(plugins.templateCache('templates.js', {
+    root: 'modules/',
+    module: 'core',
+    templateHeader: '(function () {' + endOfLine + '	\'use strict\';' + endOfLine + endOfLine + '	angular' + endOfLine + '		.module(\'<%= module %>\'<%= standalone %>)' + endOfLine + '		.run(templates);' + endOfLine + endOfLine + '	templates.$inject = [\'$templateCache\'];' + endOfLine + endOfLine + '	function templates($templateCache) {' + endOfLine,
+    templateBody: '		$templateCache.put(\'<%= url %>\', \'<%= contents %>\');',
+    templateFooter: '	}' + endOfLine + '})();' + endOfLine,
+    transformUrl: function (url) {
+      return url.replace(re, path.sep);
+    }
+  })).pipe(gulp.dest('build'));
 });
-
 // Mocha tests task
 gulp.task('mocha', function (done) {
   // Open mongoose connections
   var mongoose = require('./config/lib/mongoose.js');
   var error;
-
   // Connect mongoose
   mongoose.connect(function () {
     mongoose.loadModels();
     // Run the tests
-    gulp.src(testAssets.tests.server)
-      .pipe(plugins.mocha({
-        reporter: 'spec',
-        timeout: 10000
-      }))
-      .on('error', function (err) {
-        // If an error occurs, save it
-        error = err;
-      })
-      .on('end', function () {
-        // When the tests are done, disconnect mongoose and pass the error state back to gulp
-        mongoose.disconnect(function () {
-          done(error);
-        });
+    gulp.src(testAssets.tests.server).pipe(plugins.mocha({
+      reporter: 'spec',
+      timeout: 10000
+    })).on('error', function (err) {
+      // If an error occurs, save it
+      error = err;
+    }).on('end', function () {
+      // When the tests are done, disconnect mongoose and pass the error state back to gulp
+      mongoose.disconnect(function () {
+        done(error);
       });
+    });
   });
-
 });
-
 // Karma test runner task
 gulp.task('karma', function (done) {
   new KarmaServer({
@@ -184,12 +132,10 @@ gulp.task('karma', function (done) {
     singleRun: true
   }, done).start();
 });
-
 // Drops the MongoDB database, used in e2e testing
 gulp.task('dropdb', function (done) {
   // Use mongoose configuration
   var mongoose = require('./config/lib/mongoose.js');
-
   mongoose.connect(function (db) {
     db.connection.db.dropDatabase(function (err) {
       if(err) {
@@ -201,70 +147,72 @@ gulp.task('dropdb', function (done) {
     });
   });
 });
-
 // Downloads the selenium webdriver
 gulp.task('webdriver_update', webdriver_update);
-
 // Start the standalone selenium server
 // NOTE: This is not needed if you reference the
 // seleniumServerJar in your protractor.conf.js
 gulp.task('webdriver_standalone', webdriver_standalone);
-
 // Protractor test runner task
 gulp.task('protractor', ['webdriver_update'], function () {
-  gulp.src([])
-    .pipe(protractor({
-      configFile: 'protractor.conf.js'
-    }))
-    .on('end', function() {
-      console.log('E2E Testing complete');
-      // exit with success.
-      process.exit(0);
-    })
-    .on('error', function(err) {
-      console.log('E2E Tests failed');
-      process.exit(1);
-    });
+  gulp.src([]).pipe(protractor({
+    configFile: 'protractor.conf.js'
+  })).on('end', function () {
+    console.log('E2E Testing complete');
+    // exit with success.
+    process.exit(0);
+  }).on('error', function (err) {
+    console.log('E2E Tests failed');
+    process.exit(1);
+  });
 });
-
 // Lint CSS and JavaScript files.
 gulp.task('lint', function (done) {
   runSequence('sass', ['csslint', 'eslint'], done);
 });
-
 // Lint project files and minify them into two production files.
 gulp.task('build', function (done) {
   runSequence('env:dev', 'lint', ['uglify', 'cssmin'], done);
 });
-
 // Run the project tests
 gulp.task('test', function (done) {
   runSequence('env:test', 'lint', 'mocha', 'karma', 'nodemon', 'protractor', done);
 });
-
 gulp.task('test:server', function (done) {
   runSequence('env:test', 'lint', 'mocha', done);
 });
-
 gulp.task('test:client', function (done) {
   runSequence('env:test', 'lint', 'karma', done);
 });
-
 gulp.task('test:e2e', function (done) {
   runSequence('env:test', 'lint', 'dropdb', 'nodemon', 'protractor', done);
 });
-
 // Run the project in development mode
 gulp.task('default', function (done) {
   runSequence('env:dev', 'lint', ['nodemon', 'watch'], done);
 });
-
 // Run the project in debug mode
 gulp.task('debug', function (done) {
   runSequence('env:dev', 'lint', ['nodemon', 'watch'], done);
 });
-
 // Run the project in production mode
 gulp.task('prod', function (done) {
   runSequence('templatecache', 'build', 'env:prod', 'lint', ['nodemon', 'watch'], done);
+});
+gulp.task('ngdocs', [], function () {
+  var gulpDocs = require('gulp-ngdocs');
+  var assets = _.union(defaultAssets.server.gulpConfig, defaultAssets.server.allJS, defaultAssets.client.js, testAssets.tests.server, testAssets.tests.client, testAssets.tests.e2e);
+  var options = {
+    title: "TallyCTF Docs",
+  }
+  return gulp.src(assets).pipe(gulpDocs.process(options)).pipe(gulp.dest('./docs'));
+});
+gulp.task('serve_docs', function () {
+  var connect = require('gulp-connect');
+  connect.server({
+    root: 'docs',
+    livereload: false,
+    fallback: 'docs/index.html',
+    port: 8083
+  });
 });
