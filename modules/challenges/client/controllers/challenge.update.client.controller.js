3b297eb1e0b3b372d8f19e6c3b66fc2a59c13c65
Ben Carothers <btcrs@icloud.com>
2016-12-28 12:35:12 -0500

    Utilizes the fields service and creates a form used for updating and creating challenges -- creating, modifying and using predefined answers needs some work

--- modules/challenges/client/controllers/challenge.update.client.controller.js
+++ modules/challenges/client/controllers/challenge.update.client.controller.js
@@ -1,7 +1,7 @@
 'use strict'
 
-angular.module('challenges').controller('ChallengeUpdateController', ['$scope', '$stateParams', '$location', 'Authentication', '$http', 'filterFilter', 'Challenges',
-  function ($scope, $stateParams, $location, Authentication, $http, filterFilter, Challenges) {
+angular.module('challenges').controller('ChallengeUpdateController', ['$scope', '$stateParams', '$location', 'Authentication', '$http', 'filterFilter', 'Challenges', 'ChallengeForm',
+  function ($scope, $stateParams, $location, Authentication, $http, filterFilter, Challenges, ChallengeForm) {
     $scope.authentication = Authentication
     $scope.id = $stateParams.challengeId || 'new'
     $http.get('/modules/challenges/client/config/challengeTypes.json').then(
@@ -13,128 +13,34 @@ angular.module('challenges').controller('ChallengeUpdateController', ['$scope', 
     $scope.model = {answers: []};
     $scope.challenge = Challenges.get({ challengeId: $scope.id })
     $scope.challenge.$promise.then(function () {
-    $scope.model.type = $scope.challenge.challengeType
-    //Placeholder to be populated with user data
-    $scope.machines = ['AWS', 'Azure', 'Heroku', 'Local'].map(function (machine) {
-      return {name: machine, value: machine}
-    })
+      $scope.model.type = $scope.challenge.challengeType
+      $scope.model.category = $scope.challenge.category
+      $scope.model.description = $scope.challenge.description
+      $scope.model.name = $scope.challenge.name
+      $scope.model.points = $scope.challenge.points
+      $scope.model.machine = $scope.challenge.affectedMachine
+      $scope.model.answers = $scope.challenge.answers.map(function (answer) {
+        return answer.value
+      })
 
-
-    $scope.fields = [
-      {
-        className: 'row',
-        fieldGroup: [
-          {
-            className: 'col-xs-4',
-            type: 'input',
-            key: 'name',
-            templateOptions: {
-              label: 'Name'
-            }
-          },
-          {
-            className: 'col-xs-4',
-            type: 'input',
-            key: 'points',
-            templateOptions: {
-              label: 'Points'
-            }
-          },
-          {
-            className: 'col-xs-4',
-            type: 'input',
-            key: 'category',
-            templateOptions: {
-              label: 'Category'
-            },
-          }
-        ]
-      },
-      {
-        className: 'row',
-        fieldGroup: [
-          {
-            className: 'col-xs-12',
-            type: 'textarea',
-            key: 'description',
-            templateOptions: {
-              label: 'Description'
-            }
-          }
-        ]
-      },
-       {
-        className: 'row',
-        fieldGroup: [
-          {
-            className: 'col-xs-4',
-            type: 'select',
-            key: 'machine',
-            templateOptions: {
-              label: 'Affected Machine',
-              options: $scope.machines
-            }
-          },
-          {
-            className: 'col-xs-4',
-            type: 'select',
-            key: 'type',
-            templateOptions: {
-              label: 'Challenge Type',
-              options: $scope.challengeTypes
-            }
-          },
-          {
-            className: 'col-xs-4',
-            type: 'select',
-            key: 'format',
-            templateOptions: {
-              label: 'Challenge Format',
-            },
-            expressionProperties: {
-              'templateOptions.options': function () {
-                return $scope.challengeTypes.filter(function (type) {
-                  return type.value === $scope.model.type;
-                })[0].formats
-              }
-            }
-          }
-        ]
-      },
-      {
-        className: 'row',
-        fieldGroup: [
-          {
-            className: 'col-xs-12',
-            key: 'answers',
-            type: 'multiInput',
-            templateOptions: {
-              label: 'Answers',
-              inputOptions: {
-                type: 'input'
-              }
-            },
-            expressionProperties: {
-              'templateOptions.disabled': 'model.format === checkbox'
-            }
-          }
-        ]
-      }
-    ];
+      //Placeholder to be populated with user data
+      $scope.machines = ['AWS', 'Azure', 'Heroku', 'Local'].map(function (machine) {
+        return {name: machine, value: machine}
+      })
+      $scope.fields = ChallengeForm.createForm($scope.model, $scope.challenge, $scope.challengeTypes, $scope.machines)
+      $scope.model.format = $scope.challenge.challengeFormat
    })
 
     $scope.updateOrCreate = function (isValid) {
-      $scope.error = ''
-      this.addAnswer()
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'challengeForm')
-        return false
-      }
-      $scope.challenge.challengeType = $scope.challenge.type.name
-      $scope.challenge.challengeFormat = $scope.challenge.format.name
+      $scope.challenge.name = $scope.model.name
+      $scope.challenge.description = $scope.model.description
+      $scope.challenge.challengeType = $scope.model.type
+      $scope.challenge.points = $scope.model.points
+      $scope.challenge.challengeFormat = $scope.model.format
+      $scope.challenge.category = $scope.model.category
+      $scope.challenge.affectedMachine = $scope.model.machine
       $scope.challenge._id = $scope.id
-      delete $scope.challenge.formats
-      $scope.challenge.$save(function (response) {
+      Challenges.update($scope.challenge, function (response) {
         $location.path('challenges')
       }, function (errorResponse) {
         $scope.error = errorResponse.data.message
