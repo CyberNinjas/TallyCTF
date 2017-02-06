cc48cd630d09f8a45f7132d2ff18dc2e39bb8ccd
Ben Carothers <btcrs@icloud.com>
2017-02-06 16:56:21 -0500

    Separates CtfEvent filters based on usage

--- modules/ctfEvents/client/filters/ctfEvents.client.filter.js
+++ modules/ctfEvents/client/filters/ctfEvents.client.filter.js
@@ -1,12 +1,4 @@
-angular.module('ctfEvents').filter('unselected', function () {
-  return function (list, obj) {
-    return list.filter(function (item) {
-      if (obj && list && obj.indexOf(item._id) < 0) {
-        return true;
-      }
-    });
-  };
-}).filter('memberTeams', function () {
+angular.module('ctfEvents').filter('memberTeams', function () {
   return function (list, obj) {
     return list.filter(function (item) {
       for (var y = 0; y < obj.length; y++) {
@@ -21,42 +13,6 @@ angular.module('ctfEvents').filter('unselected', function () {
     return list.filter(function (item) {
       if (item.members.indexOf(obj._id) > -1) {
         return true;
-      }
-    });
-  };
-}).filter('selected', function () {
-  return function (list, obj) {
-    return list.filter(function (item) {
-      if (obj && list && obj.indexOf(item._id) >= 0) {
-        return true;
-      }
-    });
-  };
-}).filter('unselectedObject', function () {
-  return function (list, obj) {
-    return list.filter(function (item) {
-      if (obj && list) {
-        var seen = false
-        for (var x = 0; x < obj.length; x++) {
-          if ((obj[x]._id) === (item._id)) {
-            seen = true
-          }
-        }
-        if (!seen) {
-          return true;
-        }
-      }
-    })
-  };
-}).filter('selectedObject', function () {
-  return function (list, obj) {
-    return list.filter(function (item) {
-      if (obj && list) {
-        for (var y = 0; y < obj.length; y++) {
-          if (obj[y]._id === item._id) {
-            return true;
-          }
-        }
       }
     });
   };
---
+++ modules/ctfEvents/client/filters/ctfEvents.update.client.filter.js
@@ -0,0 +1,47 @@
+"use strict";
+
+angular.module('ctfEvents').filter('unselected', function () {
+  return function (list, obj) {
+    return list.filter(function (item) {
+      if (obj && list && obj.indexOf(item._id) < 0) {
+        return true;
+      }
+    });
+  };
+}).filter('selected', function () {
+  return function (list, obj) {
+    return list.filter(function (item) {
+      if (obj && list && obj.indexOf(item._id) >= 0) {
+        return true;
+      }
+    });
+  };
+}).filter('unselectedChallenge', function () {
+  return function (list, obj) {
+    return list.filter(function (item) {
+      if (obj && list) {
+        var seen = false
+        for (var x = 0; x < obj.length; x++) {
+          if ((obj[x].name) === (item.name)) {
+            seen = true
+          }
+        }
+        if (!seen) {
+          return true;
+        }
+      }
+    })
+  };
+}).filter('selectedChallenge', function () {
+  return function (list, obj) {
+    return list.filter(function (item) {
+      if (obj && list) {
+        for (var y = 0; y < obj.length; y++) {
+          if (obj[y].name === item.name) {
+            return true;
+          }
+        }
+      }
+    });
+  };
+})
